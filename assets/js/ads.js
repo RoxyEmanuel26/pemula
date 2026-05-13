9bd21b34f40e76701deba3e774573015f6351ac9
RoxyEmanuel26 <bimantara1997@yahoo.com>
2026-05-13 20:06:45 +0700

    feat: implement dynamic ad injection system and gallery rendering logic

--- assets/js/ads.js
+++ assets/js/ads.js
@@ -43,7 +43,7 @@
         {
             containerId: 'adBannerCustom',
             imageUrl: 'https://i.ibb.co/PvhvpsJM/ezgif-com-animated-gif-maker.gif',
-            linkUrl: 'https://1024terabox.com/s/1P3pDow9uJvchLquaiNmi5Q',
+            linkUrl: 'https://1024terabox.com/s/1tZgxhHvPTTfa2DFE2FS64A',
             alt: 'Download Terabox'
         }
     ];

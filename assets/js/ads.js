9817839a7da67540032925de40303b344876357c
RoxyEmanuel26 <bimantara1997@yahoo.com>
2026-06-05 01:23:07 +0700

    fix: replace old domain link with the correct missav-j.web.id link in custom banner ads configs

--- assets/js/ads.js
+++ assets/js/ads.js
@@ -43,7 +43,7 @@
         {
             containerId: 'adBannerCustom',
             imageUrl: 'https://i.ibb.co/PvhvpsJM/ezgif-com-animated-gif-maker.gif',
-            linkUrl: 'https://www.kumpulenak.my.id/',
+            linkUrl: 'https://www.missav-j.web.id',
             alt: 'Download Terabox'
         }
     ];
